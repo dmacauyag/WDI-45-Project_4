6fdc39b18428cb78ed39921804cd472b7296bde2
Daniel Macauyag <dmacauyag92@gmail.com>
2017-06-05 12:28:08 -0700

    tested strava api calls from the express server

--- app-server-client/express-jwt-api/server.js
+++ app-server-client/express-jwt-api/server.js
@@ -7,6 +7,7 @@ const
   usersRoutes = require('./routes/users.js'),
   cors = require('cors'),
   mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/letsmove',
+  strava = require('strava-v3'),
   port = process.env.PORT || 3001
 
 // connect to mongodb:
@@ -28,6 +29,20 @@ app.use(bodyParser.urlencoded({extended: false})) // interpret form data
 app.get('/', (req, res) => {
   res.json({message: "Server root. All API routes start with /api..."})
 })
+
+// testing strava API calls
+// strava.segments.get({'access_token':'23c2e332af8d3fe34dadf3215ca46ab2c57c5752', 'id': '229781'}, (err,payload,limits) => {
+//   if (err) return console.log(err)
+//   console.log('data', payload)
+//   console.log('limits', limits)
+// })
+
+strava.segments.explore({'access_token':'23c2e332af8d3fe34dadf3215ca46ab2c57c5752', 'bounds': '37.821362,-122.505373,37.842038,-122.465977'}, (err,payload,limits) => {
+  if (err) return console.log(err)
+  console.log('data', payload)
+  console.log('limits', limits)
+})
+
 
 // apply all user routes here:
 app.use('/api/users', usersRoutes)
