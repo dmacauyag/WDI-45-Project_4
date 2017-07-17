f7e139fc7ca58138901b4c91742a81564a1db10f
Daniel Macauyag <dmacauyag92@gmail.com>
2017-07-17 12:25:55 -0700

    added min and max climb category parameters to strava api request

--- routes/strava.js
+++ routes/strava.js
@@ -6,7 +6,8 @@ const
 
 stravaRouter.post('/segments', (req, res) => {
   console.log('req data sent for map bounds:', req.body.boundary)
-  strava.segments.explore({'access_token':'23c2e332af8d3fe34dadf3215ca46ab2c57c5752', 'bounds': req.body.boundary, 'activity_type': req.body.activityType}, (err, payload, limits) => {
+  console.log('req data sent:', req.body)
+  strava.segments.explore({'access_token':'23c2e332af8d3fe34dadf3215ca46ab2c57c5752', 'bounds': req.body.boundary, 'activity_type': req.body.activityType, 'min_cat': req.body.minClimbCat, 'max_cat': req.body.maxClimbCat}, (err, payload, limits) => {
     if (err) return console.log(err)
     console.log('data', payload)
     console.log('limits', limits)
